a4b49053ccbfb32f7f7851594a9ec1b93110ac76
Ghulaa <165237428+Ghulaa@users.noreply.github.com>
2024-05-06 12:56:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="samsamsun789@gmail.com"
 global.location="Lahore,Pakistan."
 
 
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongodb+srv://kajadex292:kajadex292@cluster0.ptwm1vv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
